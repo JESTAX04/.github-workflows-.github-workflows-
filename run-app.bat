2d2af148d81af1aad231c2a80c992d43d714376d
JESTAX04 <theekshanasdaruvana25@gmail.com>
2026-02-12 18:33:22 +0530

    Delete run-app.bat

--- run-app.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-if exist ".tools\node-v20.11.1-win-x64\node.exe" (
-  set "PATH=%CD%\.tools\node-v20.11.1-win-x64;%CD%\.tools\node-v20.11.1-win-x64\node_modules\npm\bin;%PATH%"
-)
-npm start
